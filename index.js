481673688520013033df2e73bee77fb8c8a2ed3c
Zahid Patoary <zahidpatoary@Zahids-MacBook-Air.local>
2022-08-05 11:23:47 +0900

    get all inventory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 4000;
 const app = express();
@@ -9,6 +9,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5hyer.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        await client.connect();
+        const carCollection = client.db('pkCars').collection('products');
+
+        // get all inventory
+        app.get('/products', async(req,res) =>{
+            const query = {};
+            const cursor = carCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+        
+    }
+}
+ run().catch(console.dir);
 
 app.get('/', (req, res) =>{
     res.send('pkcars connected');
